f333ab0e424bc399c3f38b34d065ebcf34dacf23
nikoloz-kupravishvili <spidernikkaa@gmail.com>
2024-06-29 13:03:36 +0400

    third commit

---
+++ day 02/homework/friends.py
@@ -0,0 +1,7 @@
+new_friends = ['elene aribovi',"nikoloz tsereteli","luka matcaberidze"]
+
+
+def call_friends():
+    print(f"new friends are: {new_friends[0]} ,{new_friends[1]} and {new_friends[2]}")
+
+call_friends()
